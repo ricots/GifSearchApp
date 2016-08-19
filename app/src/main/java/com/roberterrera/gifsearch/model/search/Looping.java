f4e6d15b0129542d9b757bcf782475b182a2590c
roberrera <roberterrera@gmail.com>
2016-08-19 00:44:05 -0400

    Create Search API call, add pojo GSON glasses,

---
+++ app/src/main/java/com/roberterrera/gifsearch/model/search/Looping.java
@@ -0,0 +1,34 @@
+
+package com.roberterrera.gifsearch.model.search;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import javax.annotation.Generated;
+
+@Generated("org.jsonschema2pojo")
+public class Looping {
+
+    @SerializedName("mp4")
+    @Expose
+    private String mp4;
+
+    /**
+     * 
+     * @return
+     *     The mp4
+     */
+    public String getMp4() {
+        return mp4;
+    }
+
+    /**
+     * 
+     * @param mp4
+     *     The mp4
+     */
+    public void setMp4(String mp4) {
+        this.mp4 = mp4;
+    }
+
+}
--- app/src/main/java/com/roberterrera/gifsearch/model/service/GiphyAPIService.java
+++ app/src/main/java/com/roberterrera/gifsearch/model/service/GiphyAPIService.java
@@ -1,9 +1,11 @@
 package com.roberterrera.gifsearch.model.service;
 
+import com.roberterrera.gifsearch.model.search.SearchResponse;
 import com.roberterrera.gifsearch.model.trending.TrendingResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 /**
  * Created by Rob on 8/9/16.
@@ -14,4 +16,7 @@ public interface GiphyAPIService {
     /* Because we are using the public API key it can go right in here,*/
     @GET("trending?rating=pg&fmt=json&api_key=dc6zaTOxFJmzC")
     Call<TrendingResponse> getTrending();
+
+    @GET("search?fmt=json&rating=pg&api_key=dc6zaTOxFJmzC")
+    Call<SearchResponse> searchRequest(@Query("q") String query);
 }
